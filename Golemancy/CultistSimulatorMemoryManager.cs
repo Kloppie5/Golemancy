66920397b940a603796d676e6c92843661d0242f
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2021-05-23 18:12:15 +0200

    Grab TabletopManager

---
+++ Golemancy/CultistSimulatorMemoryManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Golemancy {
+	class CultistSimulatorMemoryManager {
+		[DllImport("kernel32.dll")]
+	    public static extern Boolean ReadProcessMemory( IntPtr hProcess, IntPtr lpBaseAddress, Byte[] lpBuffer, Int32 dwSize, out Int32 lpNumberOfBytesRead );
+	    [DllImport("kernel32.dll")]
+	    public static extern Boolean WriteProcessMemory( IntPtr hProcess, IntPtr lpBaseAddress, Byte[] lpBuffer, Int32 dwSize, out Int32 lpNumberOfBytesWritten );
+
+		/**
+			Generic rant 1; Who in their right mind thought it would be a good idea to hide the complex yet beautiful C++ types with the mess that is IntPtr and its ramifications for 64-bit code that interacts with 32-bit code?
+			Generic rant 2; I know that array indexing is basically just syntactic sugar for pointer arithmetic, but I still think it's a terrible design choice to make array indexing require signed types.
+		*/
+
+		public UInt32 FindTabletopManager ( Process process ) {
+			UInt32 start = 0x08CF0000;
+			UInt32 stop  = 0x09000000;
+
+			Byte[] region = new Byte[stop-start];
+			ReadProcessMemory(process.Handle, (IntPtr) start, region, (Int32) (stop-start), out Int32 lpNumberOfBytesRead);
+
+			for ( Int32 c = 0 ; c < stop-start-8 ; c += 4 ) {
+
+				if (
+					BitConverter.ToUInt32(region, c + 0) == 0x19355C58 // vtable
+				&&  BitConverter.ToUInt32(region, c + 4) == 0x00000000 // Unity alignment bytes
+				&&  BitConverter.ToUInt32(region, c + 8) != 0x00000000 // UnityEngine.Object.m_CachedPtr
+				) {
+					return start + (UInt32) c;
+				}
+			}
+
+			return 0;
+		}
+
+		public static T Read<T> ( Process process, UInt32 address ) where T : struct {
+			Int32 size = Marshal.SizeOf(typeof(T));
+			Byte[] read = new Byte[size];
+
+			ReadProcessMemory(process.Handle, (IntPtr) address, read, size, out Int32 lpNumberOfBytesRead);
+
+			T result;
+			GCHandle handle = GCHandle.Alloc(read, GCHandleType.Pinned);
+
+			try {
+				result = (T) Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(T));
+			} finally {
+				handle.Free();
+			}
+
+			return result;
+		}
+	}
+}
--- Golemancy/Program.cs
+++ Golemancy/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -10,12 +12,44 @@ namespace Golemancy {
 
 		public static String AppID = "718670";
 		public static String BuildID = "6212706";
-	}
+	}	
 
 	static class Program {
 		[STAThread]
 		static void Main() {
 			Console.WriteLine($"Initializing Golemancy v{METADATA.Version} for Cultist Simulator, build {METADATA.BuildID}");
+
+			Process process = Process.GetProcessesByName("cultistsimulator").FirstOrDefault();
+            Console.WriteLine("process: " + process);
+
+            IntPtr processHandle = process.Handle;
+            Console.WriteLine("Process Handle: " + processHandle);
+
+            CultistSimulatorMemoryManager csmm = new CultistSimulatorMemoryManager();
+            UInt32 ttm = csmm.FindTabletopManager(process);
+            Console.WriteLine($"Found TabletopManager at {ttm:X8}");
+
+
+
+            Console.ReadLine();
+
+            /**
+            
+            Assets.CS.TabletopUI.Registry
+            
+            
+            A Situation is wrapped in a SituationController
+
+            A SituationController has a link back to the current Character
+
+            TabletopTokenContainer
+            contains Choreagrapher
+            Token dictionary
+
+            TabletopManager
+            contains TabletopTokenContainer
+            contains MapController
+             */
 
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
