5bdc104b7a6006b2c8b62dc84f94f9d664835603
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2024-07-14 17:53:34 +0200

    Knock knock Mansus; you have no walls and I must come in

--- Golemancy/Program.cs
+++ Golemancy/Program.cs
@@ -1,8 +1,18 @@
 ﻿
+using System.Diagnostics;
+
 namespace Golemancy;
 
 class Program {
     public static void Main ( ) {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Knock knock Mansus");
+
+        var process = Process.GetProcesses().FirstOrDefault(p => p.ProcessName == "cultistsimulator");
+        if (process is null)
+        {
+            Console.WriteLine("[X] Could not find Process");
+            return;
+        }
+        Console.WriteLine($"Found process {process.Id}");
     }
 }
