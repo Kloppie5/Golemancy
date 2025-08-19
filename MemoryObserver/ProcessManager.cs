e4d94e4e69809daced881397a428e2b52bc2fde5
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2025-08-19 21:27:08 +0200

    Setting up an HTTP controlled ProcessManager

---
+++ MemoryObserver/ProcessManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+
+namespace MemoryObserver;
+
+public class ProcessManager
+{
+    public ProcessManager(Process process)
+    {
+        Console.WriteLine($"Initializing ProcessManager for process {process.ProcessName} ({process.Id})");
+    }
+}
---
+++ MemoryObserver/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryObserver;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Process[] processes = Process.GetProcessesByName("cultistsimulator");
+        var pm = new ProcessManager(processes[0]);
+
+        var listener = new HttpListener();
+        listener.Prefixes.Add("http://localhost:5000/");
+        listener.Start();
+        Console.WriteLine("Started listening to HTTP requests on port 5000");
+
+        while (true)
+        {
+            var ctx = listener.GetContext();
+            var req = ctx.Request;
+            var res = ctx.Response;
+
+            try
+            {
+                Console.WriteLine($"Got something on {req.Url.AbsolutePath}");
+                Console.WriteLine(req.QueryString);
+            }
+            catch (Exception ex)
+            {
+                res.StatusCode = 500;
+                var buf = System.Text.Encoding.UTF8.GetBytes(ex.ToString());
+                res.OutputStream.Write(buf, 0, buf.Length);
+            }
+            res.OutputStream.Close();
+        }
+    }
+}
